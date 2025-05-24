3d070045d4e1534b59abcdf64c4dad7bfa9e67ee
vamsiavk2212 <vamsiavk2208@gmail.com>
2025-05-24 12:03:10 +0530

    new-instance

--- roboshop.sh
+++ roboshop.sh
@@ -11,14 +11,14 @@ for instance in ${INSTANCES[@]}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro 
     --security-group-ids sg-0e757d5b59ac720a8 --tag-specifications "ResourceType=instance,
-    Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != frontend]
+    Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
     then
-        aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 
-        "Reservations[0].Instances[0].PrivateIpAddress" --output text
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 
+        "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
-        aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 
-        "Reservations[0].Instances[0].PublicIpAddress" --output text
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 
+        "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
     echo "$INSTANCE_ID IP address: $IP"
 done
