7e7554e4538cf82ab8d13a23e1ecdebf7afec890
vamsiavk2212 <vamsiavk2208@gmail.com>
2025-05-24 13:27:46 +0530

    instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -1,34 +1,19 @@
-#!/bin/bash
-
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0e757d5b59ac720a8"
-INSTANCES=("MONGODB" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z08593462361SABLJQHSK"
-DOMAIN_NAME="daws100s.site"
+SG_ID="sg-0e757d5b59ac720a8" 
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z08593462361SABLJQHSK" 
+DOMAIN_NAME="daws100s.site" 
 
-for instance in "${INSTANCES[@]}"
+for instance in ${INSTANCES[@]}
 do
-    echo "Launching instance: $instance"
-    INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id "$AMI_ID" \
-        --instance-type t3.micro \
-        --security-group-ids "$SG_ID" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-        --query "Instances[0].InstanceId" \
-        --output text)
-
-    echo "Waiting for instance $INSTANCE_ID to be in 'running' state..."
-    aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
-
-    if [ "$instance" != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" \
-            --query "Reservations[0].Instances[0].PrivateIpAddress" \
-            --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0e757d5b59ac720a8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" \
-            --query "Reservations[0].Instances[0].PublicIpAddress" \
-            --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
-
-    echo "$instance instance ($INSTANCE_ID) IP address: $IP"
+    echo "$instance IP address: $IP"
 done
